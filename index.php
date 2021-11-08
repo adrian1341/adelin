1a22a0f2b80ae6bf24cb531a9c9f1a15a85f6b83
adrian1341 <cismelaru.adelin2@gmail.com>
2021-11-08 19:13:49 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    </head>
+    <body>
+        <!-- banner -->
+        <?php
+        //momentan comentati/decomentati unul din template-uri in functie de sectiunea la care vreti sa lucrati
+            require_once 'templates/template_conectat.php';
+//            require_once 'templates/template_neconectat.php';
+        ?>
+    </body>
+</html>
